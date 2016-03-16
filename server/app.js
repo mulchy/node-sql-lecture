0ce3eba91318cb01f7cb28410c4ea4d78a57d822
Ryan Mulcahy <ryan@primeacademy.io>
2016-03-16 11:45:17 -0500

    Updated app to run on Heroku

--- server/app.js
+++ server/app.js
@@ -3,7 +3,38 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var pg = require('pg');
 
-var connectionString = 'postgres://localhost:5432/tuesday';
+var connectionString;
+
+if (process.env.DATABASE_URL) {
+  pg.defaults.ssl = true;
+  connectionString = process.env.DATABASE_URL;
+} else {
+  connectionString = 'postgres://localhost:5432/tuesday';
+}
+
+pg.connect(connectionString, function(err, client, done){
+  if (err) {
+    console.log('Error connecting to DB!', err);
+    //TODO end process with error code
+  } else {
+    var query = client.query('CREATE TABLE IF NOT EXISTS people (' +
+                              'id SERIAL PRIMARY KEY,' +
+                              'name varchar(80) NOT NULL,' +
+                              'address text);'
+    );
+
+    query.on('end', function(){
+      console.log('Successfully ensured schema exists');
+      done();
+    });
+
+    query.on('error', function() {
+      console.log('Error creating schema!');
+      //TODO exit(1)
+      done();
+    });
+  }
+});
 
 var app = express();
 
